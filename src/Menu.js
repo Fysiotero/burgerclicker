4a330ce52e76d09ea0e58b2bc64711c8db5cd32d
Tero Silvennoinen <tero.t.silvennoinen@gmail.com>
2019-04-19 14:23:47 +0300

    Reititys lisätty

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import iconburger from './Images/icon-burger.png';
 import iconcoupon from './Images/icon-coupon.png';
 import iconboy from './Images/icon-boy.png';
@@ -6,9 +8,9 @@ import iconboy from './Images/icon-boy.png';
 function Menu(props) {
     return (
         <div className="menu">
-        <div><img src={iconburger} alt="game" /></div>
-        <div><img src={iconcoupon} alt="coupons" /><span className="badge">{props.claimableCoupons}</span></div>
-        <div><img src={iconboy} alt="profile" /></div>
+        <Link to="/"><div><img src={iconburger} alt="game" /></div></Link>
+        <Link to="/coupons"><div><img src={iconcoupon} alt="coupons" /><span className="badge">{props.claimableCoupons}</span></div></Link>
+        <Link to="/profile"><div><img src={iconboy} alt="profile" /></div></Link>
         </div>
     );
 }
